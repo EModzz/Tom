d3ce2020782444aafa9f42c841ec82498e7776d3
Tom ツ <50146042+EModzz@users.noreply.github.com>
2022-11-23 15:26:12 +0100

    Ex 7.2
    Update

---
+++ laboprogra/Ex7.2/ST_Ex7.2.js
@@ -0,0 +1,16 @@
+var Note = prompt("Qu'elle note as-tu obtenue?");
+if (Note >= 90){
+    document.write("La plus grande distinction!");
+} else if (Note >= 80){
+    document.write("Grande distinction!");
+} else if (Note >= 70){
+    document.write("Distinction!");
+} else if (Note >= 70){
+    document.write("Distinction!");
+} else if (Note >= 60){
+    document.write("Satisfaction!");
+} else if (Note >= 50){
+    document.write("Réussite!");
+} else if (50 > Note){
+    document.write("Échec!");
+}
